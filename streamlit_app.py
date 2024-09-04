79fe0ea68fbc7014e15b5373031cc9be8756f2e8
azkapublik <azkapublik@gmail.com>
2024-09-05 04:31:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ else:
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
         with st.chat_message("assistant"):
-            response = st.write_stream(data)
+            response = st.write_stream(data['data']['output'])
         st.session_state.messages.append({"role": "assistant", "content": response})
